d054abfe6bb841b1935571d63e2ab2aa905df91d
Guruprasad Gaonkar <ggaonk@codeaurora.org>
2011-12-02 18:36:55 -0800

    Camera : Changes to Disable Panoroma Menu from UI
    Change-Id: I74e3515e208950d3a1a7400ffe662330c49e2a5b

--- src/com/android/camera/VideoCamera.java
+++ src/com/android/camera/VideoCamera.java
@@ -1528,12 +1528,13 @@ public class VideoCamera extends ActivityBase
                 switchToOtherMode(ModePicker.MODE_CAMERA);
             }
         });
+/*
         MenuHelper.addSwitchModeMenuItem(menu, ModePicker.MODE_PANORAMA, new Runnable() {
             public void run() {
                 switchToOtherMode(ModePicker.MODE_PANORAMA);
             }
         });
-
+*/
         if (mNumberOfCameras > 1) {
             menu.add(R.string.switch_camera_id)
                     .setOnMenuItemClickListener(new OnMenuItemClickListener() {
